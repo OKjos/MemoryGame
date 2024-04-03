215e3be81d20015d314bcd9623dfa03a3abfc869
Joshua <113487918+OKjos@users.noreply.github.com>
2024-04-02 20:02:55 -0400

    Bit of JS

--- Memory.js
+++ Memory.js
@@ -5,20 +5,75 @@
 //check to see if emojis match
 //if they dont match turn the back around
 //if they do match keep them showing and allow 2 more clicks
-const symbols = ["☁", "💻", "🌎", "🔌", "⚙", "🔒", "📦", "🔧"]
+const symbols = ["☁", "☁", "💻", "💻", "🌎", "🌎", "🔌", "🔌", "⚙", "⚙", "🔒", "🔒", "📦", "📦","🔧", "🔧"]
 
-let emojiSet;
-let board = [];
+// cards, moves, restart, result query
+const cards = document.querySelector('.card');
+const moves = document.querySelector('.moves');
+const restart = document.querySelector('.restart');
+const result = document.querySelector('.result');
 
-window.onload = function() {
-  shuffleEmojis();
+
+function createCardGrid() {
+  const cardGrid = document.querySelector('.button-grid');
+  emojiSet.forEach((emoji, index) => {
+    const card = document.querySelector('div');
+    card.classList.add('card');
+    card.dataset.index = index;
+    card.addEventListener('click', flipCard);
+    card.innerHTML = `<div class="front"><div class="emoji">${emoji}</div></div>`;
+    cardGrid.appendChild(card);
+  })
+}
+
+createCardGrid();
+
+function shuffleEmojis() {
+  emojiSet = symbols.concat(symbols);
+  for (let i = emojiSet.length - 1; i > 0; i--) {    
+    let j = Math.floor(Math.random() * (i - 1));
+    [emojiSet[i], emojiSet[j]] = [emojiSet[j], emojiSet[i]];
+  }
+}
+
+shuffleEmojis();
+
+
+let firstCard, secondCard;
+let  board = [];
+
+function flipCard() {
+  if (lockBoard) return;
+  if (this === firstCard) return;
+
+  this.classList.add('flipped');
+
+  if (!fistCard) {
+    firstCard = this;
+    return;
+  }
+
+  secondCard = this;
+
+  checkForMatch();
+}
+
+function checkForMatch() {
 
 }
 
-function shuffleEmojis() {
-  emojiSet = symbols.concat(emojiList);
+function disableCards() {
 
-  for (let i = 0; i < emojiSet.length; i++) {
-    let j = Math.floor(Math.random() * emojiSet.length)
-  }
+}
+
+function resetBoard() {
+
+}
+
+function restartGame() {
+
+}
+
+function shuffle() {
+
 }
